b497ad0c71faf61c49d0ec9529aeb5b77a1d5097
DharuNamikaze <dharunamikaze@gmail.com>
2024-11-14 09:49:42 +0530

    Landing page changes

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -3,19 +3,18 @@ import React from 'react';
 
 const LandingPage = ({ startGame }) => {
   return (
-    <>
-    <div className="landing-page">
-      <h1 className="text-4xl font-bold mt-10 text-center">Chronicles of You</h1>
-      <p className="text-lg m-4 text-center">Embark on an epic adventure where every choice shapes your story.</p>
-      <button
-        className="py-3 px-6 mt-4 flex text-center bg-blue-500 text-white rounded-full text-xl"
-        onClick={startGame}
-      >
-        Start Game
-      </button>
+    <div className="landing-page flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mt-10">Chronicles of You</h1>
+        <p className="text-lg m-2">Embark on an epic adventure where every choice shapes your story.</p>
+        <button
+          className="py-3 px-6 mt-2 bg-blue-500 text-white rounded-full text-xl"
+          onClick={startGame}
+        >
+          Start Game
+        </button>
+      </div>
     </div>
-    </>
-    
   );
 };
 
